526a2076df62f324983a2c3f8169c466bb19bd58
Kike Signes <devksig@gmail.com>
2026-01-15 10:56:12 +0100

    Add TransmissionError with localized description.

---
+++ TelegraphSimulator/Domain/TransmissionError.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+/// Definición de los errores
+enum TransmissionError: Error {
+    case signalLost(at: String)
+    case componentFailure(id: String, details: String)
+    case decodingError(details: String)
+}
+
+/// Separamos la lógica de presentación del error.
+extension TransmissionError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .signalLost(at: let location):
+            return "ERROR: Señal degradada irreversiblemente en \(location)"
+        case .componentFailure(id: let id, details: let details):
+            return "ERROR: El componente \(id) ha fallado: \(details)"
+        case .decodingError(details: let details):
+            return "ERROR: Error de decodificación: \(details)"
+        }
+    }
+}
