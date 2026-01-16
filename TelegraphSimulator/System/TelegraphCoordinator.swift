d512298a51f3e1b637ebcfc482fdcde1d9fecb5a
Kike Signes <devksig@gmail.com>
2026-01-16 21:32:27 +0100

    Implement TelegraphCoordinator.

---
+++ TelegraphSimulator/System/TelegraphCoordinator.swift
@@ -0,0 +1,52 @@
+import Foundation
+
+class TelegraphCoordinator {
+    private var emitter: any MessageEmitter
+    private var receiver: any MessageReceiver
+    private let network: [any SignalTransmitter]
+
+    init(emitter: any MessageEmitter, receiver: any MessageReceiver, network: [any SignalTransmitter]) {
+        self.emitter = emitter
+        self.receiver = receiver
+        self.network = network
+    }
+
+    func send(message: String) async {
+        print("INICIANDO TRANSMISIÓN")
+
+        emitter.turnOn()
+
+        // El emisor comienza el envío del mensaje.
+        var currentSignal = emitter.emit(message: message)
+
+        // La señal pasa através de la red de canales y relés.
+        for component in network {
+
+            let result = await component.transmit(currentSignal)
+
+            // Se comprueba que la señal pasa a través de todos los componentes de la red, fallando en caso de error.
+            switch result {
+            case .success(let newSignal):
+                currentSignal = newSignal
+            case .failure(let error):
+                print(error.localizedDescription)
+                emitter.turnOff()
+                return
+            }
+        }
+
+        // La señal llega al receptor
+        let finalResult = receiver.receive(currentSignal)
+
+        // Se obtiene el resultado final de la transmissión.
+        switch finalResult {
+        case .success(let decodedMessage):
+            print("Mensaje recibido correctamente: \(decodedMessage)")
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+        
+        emitter.turnOff()
+        print("FIN DE LA TRANSMISIÓN")
+    }
+}
